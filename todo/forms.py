d1c598fdf93f2338feedaef4e5583fc9d6728b2c
Александр Смолин <smolll123@mail.ru>
2021-03-23 20:39:17 +0300

    Add new form class TodoItemForm to forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from .models import TodoItem
+
+class TodoItemForm(forms.ModelForm):
+    content = forms.CharField
+
+    class Meta:
+        model = TodoItem
+        fields = [
+            "content",
+        ]
