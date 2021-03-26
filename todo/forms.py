43c3b6e421ff9a5b124d31fa05a5c50fd629dd64
Александр Смолин <smolll123@mail.ru>
2021-03-26 18:18:18 +0300

    Added empty label attribute to TodoItemForm

--- todo/forms.py
+++ todo/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import TodoItem
 
 class TodoItemForm(forms.ModelForm):
-    content = forms.CharField
+    content = forms.CharField(label='')
 
     class Meta:
         model = TodoItem
