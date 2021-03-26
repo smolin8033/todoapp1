4c2424dcec6f77607b175658331d37a0213cae86
Александр Смолин <smolll123@mail.ru>
2021-03-26 16:51:29 +0300

    created todoView function, showing list of objects

--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,8 @@ from .models import TodoItem
 # Create your views here.
 
 def todoView(request):
-    return render(request, "todo.html", {})
+    queryset = TodoItem.objects.all()
+    context = {
+        "object_list": queryset,
+    }
+    return render(request, "todo.html", context)
