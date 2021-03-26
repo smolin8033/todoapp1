c5bb91bd852070adfbfede3b0db384528de2de9c
Александр Смолин <smolll123@mail.ru>
2021-03-26 18:14:18 +0300

    Imported createView to urls.py

--- todoapp_root/urls.py
+++ todoapp_root/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import todoView
+from todo.views import todoView, createView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
