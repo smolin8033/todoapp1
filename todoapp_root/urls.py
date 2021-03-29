bf758c9fcb6f12a40da258b9a5b329f37cf89781
Александр Смолин <smolll123@mail.ru>
2021-03-29 17:56:46 +0300

    Imported deleteView

--- todoapp_root/urls.py
+++ todoapp_root/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import todoView, createView
+from todo.views import todoView, createView, deleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
